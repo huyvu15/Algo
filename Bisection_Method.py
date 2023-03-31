5d74267a60d4e02f3e2dd79f60f38208c2a33a46
VuHuy <huyv80313@gmail.com>
2023-03-31 16:30:25 +0700

    upload Bisection method

---
+++ Bisection_Method.py
@@ -0,0 +1,37 @@
+# bisection method
+
+import math
+
+print("Tìm nghiệm của hàm số f(x) = x^x -10 trên khoảng: ")
+# (a, b)
+print("Nhap vao khoang (a, b): ", end = "")
+a, b = map(int, input().split())
+
+# enter error value
+
+error = float(input("Nhap vao sai so: "))
+
+n = float(math.ceil(math.log((b-a)*(error**(-1)), 2)))
+
+print("số lần lặp là: ", int(n))
+
+def f(x):
+    return x**x-10
+
+def test(a, b):
+    i = 0
+    while abs(b - a) >= error:
+        c = (a+b)/2
+        if f(a)*f(c) < 0:
+            b = c
+            print(f"lần {i}: a = {a}, b = {b}, c = {c}, f(c) = ", f(c))
+        else:
+            a = c
+            print(f"lần {i}: a = {a}, b = {b}, c = {c}, f(c) = ", f(c))
+        i +=1
+    return c   
+print("Cách thuật toán chạy:")
+print("Nghiệm của phương trình là: ",test(a, b))
+
+
+
